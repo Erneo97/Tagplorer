6d9f0b21523fe2695afcb7cfb3c42ffeaea8f9cc
Erneo97 <michalkaniewski1997@gmail.com>
2026-05-16 12:20:15 +0200

    Test działana watchEvent

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,47 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String testDir = System.getenv("testDir").replace('\'', ' ').trim();
+        System.out.println("Test dir path: " + testDir);
 
+        Path pathDadDir = Paths.get(testDir);
+        try {
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+
+            pathDadDir.register(watchService,
+                    StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_DELETE,
+                    StandardWatchEventKinds.ENTRY_MODIFY
+            );
+
+            System.out.println("Monitoring folderu: " + pathDadDir);
+
+            WatchKey key;
+            while ((key = watchService.take()) != null) {
+
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    WatchEvent.Kind<?> kind = event.kind();
+                    Path path = (Path) event.context();
+
+                    System.out.println(kind.name() + " - " + path + "  " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
+                    File file = pathDadDir.resolve(path).toFile();
+                    System.out.println("\t" + file.getAbsolutePath() + "   " + file.isDirectory() + "   " + file.isFile() + "   " );
+
+                }
+
+                key.reset();
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
