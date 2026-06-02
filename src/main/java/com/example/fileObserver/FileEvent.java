73db32d56e1855332f279f575e1fec1793d08d92
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-02 13:13:08 +0200

    feat: add time field to the class generate methods equals and hasCode

--- src/main/java/com/example/fileObserver/FileEvent.java
+++ src/main/java/com/example/fileObserver/FileEvent.java
@@ -1,14 +1,18 @@
 package com.example.fileObserver;
 
 import java.nio.file.Path;
+import java.time.LocalTime;
+import java.util.Objects;
 
 public class FileEvent {
     private final ExpandedWatchEventsKids type;
     private final Path path;
+    private final LocalTime time;
 
-    public FileEvent(ExpandedWatchEventsKids type, Path path) {
+    public FileEvent(ExpandedWatchEventsKids type, Path path, LocalTime time) {
         this.type = type;
         this.path = path;
+        this.time = time;
     }
 
     public ExpandedWatchEventsKids getType() {
@@ -17,5 +21,18 @@ public class FileEvent {
 
     public Path getPath() {
         return path;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        FileEvent fileEvent = (FileEvent) o;
+        return type == fileEvent.type && Objects.equals(path, fileEvent.path) && Objects.equals(time, fileEvent.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, path, time);
     }
 }
