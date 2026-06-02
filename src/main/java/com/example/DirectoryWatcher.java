82f31680961637b7146aa40b622718b4778230d7
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-02 13:16:51 +0200

    feat: add publisher to DirectoryWahtcher

--- src/main/java/com/example/DirectoryWatcher.java
+++ src/main/java/com/example/DirectoryWatcher.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import com.example.fileObserver.ExpandedWatchEventsKids;
+import com.example.fileObserver.FileEvent;
+import com.example.fileObserver.FileEventPublisher;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
@@ -11,22 +15,22 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class DirectoryWatcher implements Runnable{
     private final Path path;
     private final AtomicBoolean run;
+    private final FileEventPublisher publisher;
     WatchService watchService;
 
 
-    public DirectoryWatcher(Path path) throws IOException {
+    public DirectoryWatcher(Path path, FileEventPublisher publisher) throws IOException {
         this.path = path;
         run = new AtomicBoolean(true);
         watchService =  FileSystems.getDefault().newWatchService();
+        this.publisher = publisher;
 
         path.register(watchService,
                     StandardWatchEventKinds.ENTRY_CREATE,
                     StandardWatchEventKinds.ENTRY_DELETE,
                     StandardWatchEventKinds.ENTRY_MODIFY
             );
-
     }
-
 
     @Override
     public void run() {
@@ -38,9 +42,16 @@ public class DirectoryWatcher implements Runnable{
                         WatchEvent.Kind<?> kind = event.kind();
                         Path path = (Path) event.context();
 
-                        System.out.println(kind.name() + " - \'" + path + "\'  " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
+                        System.out.println(kind.name() + " - '" + path + "'  " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
                         File file = this.path.resolve(path).toFile();
                         System.out.println("\t" + file.getAbsolutePath() + "   " + file.isDirectory() + "   " + file.isFile() + "   " );
+
+                        FileEvent newEvent = new FileEvent(
+                                ExpandedWatchEventsKids.valueOf( kind.name()),
+                                path,
+                                LocalTime.now()
+                        );
+                        publisher.publish(newEvent);
 
                     }
                     key.reset();
